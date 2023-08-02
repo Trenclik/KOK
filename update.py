cdcbe08bc5eb3d806dce850f55b19affd2b9321a
Trenclik <trenclikb1@gmail.com>
2023-08-02 02:49:04 +0200

    oprava updatu 2
    kok

--- update.py
+++ update.py
@@ -4,8 +4,8 @@ import os
 import subprocess
 import sys
 
-APP_VERSION = "1.0.0"  # Replace this with your app's current version
-GITHUB_REPO_URL = "https://api.github.com/repos/Trenclik/KOK/releases/latest"
+APP_VERSION = "v1.2.0"  # Replace this with your app's current version
+GITHUB_REPO_URL = "https://api.github.com/repos/Trenclik/KOK/releases"
 HEADERS = {
     "Authorization": "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" #NEMAZAT!!!!!! JE TO API KLÍČ!!!!!!
 }
@@ -14,11 +14,18 @@ def check_for_updates():
     try:
         response = requests.get(GITHUB_REPO_URL, headers=HEADERS)
         response.raise_for_status()
-        release_data = response.json()
-        latest_version = release_data["tag_name"]
-        return latest_version
-    except (requests.exceptions.RequestException, KeyError):
-        pass
+        releases_data = response.json()
+        
+        if releases_data:
+            latest_release = releases_data[0]
+            latest_version = latest_release["tag_name"]
+            return latest_version
+
+    except requests.exceptions.RequestException as e:
+        print("Error occurred during API request:", e)
+    except KeyError as e:
+        print("Error parsing response data:", e)
+
     return None
 
 def update_app(latest_version):
@@ -48,8 +55,15 @@ def update_app(latest_version):
 
 if __name__ == "__main__":
     latest_version = check_for_updates()
+    print("Latest version from GitHub:", latest_version)
+    print("Current app version:", APP_VERSION)
+    latest_version = check_for_updates()
     if latest_version and latest_version != APP_VERSION:
         update_app(latest_version)
     else:
         print("No updates available. Running the app...")
         # Your app's main code goes here
+
+response = requests.get(GITHUB_REPO_URL, headers=HEADERS)
+print(response.json())
+print(response.status_code)  # Check the HTTP status code
