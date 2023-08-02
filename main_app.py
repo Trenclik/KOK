69f8239f74f2886ef8fcf4a1cf808221ada5ce13
Trenclik <trenclikb1@gmail.com>
2023-08-02 15:14:10 +0200

    malý opravy

--- main_app.py
+++ main_app.py
@@ -1,4 +1,7 @@
 import subprocess
+import os
+script_directory = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_directory)
 def execute_python_script(script_path):
     try:
         result = subprocess.run(["python", script_path], capture_output=True, text=True)
@@ -10,7 +13,5 @@ def execute_python_script(script_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 if __name__ == "__main__":
-    script1_path = "řešení.py"
-    script2_path = "kok.py"
+    script1_path = "update.py"
     execute_python_script(script1_path)
-    execute_python_script(script2_path)
